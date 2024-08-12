d03e00f3b041e9cad3cd348ac97f1d35106c6e35
Sebastian Krüger <sk@honeymachine.io>
2024-08-13 01:04:12 +0200

    feat: shairport

---
+++ host/shairport.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+sudo apt-get install --no-install-recommends build-essential git autoconf automake libtool \
+    libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev \
+    libplist-dev libsodium-dev libavutil-dev libavcodec-dev libavformat-dev uuid-dev libgcrypt-dev xxd -y
+
+git clone https://github.com/mikebrady/shairport-sync.git ./tmp/shairport-sync
+cd ./tmp/shairport-sync
+autoreconf -fi
+./configure --sysconfdir=/etc --with-alsa \
+    --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-airplay-2
+make
+# make install
