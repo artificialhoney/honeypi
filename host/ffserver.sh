8dd8c946315bf3635808ef7e6cb7d195c84ac887
Sebastian Krüger <sk@honeymachine.io>
2024-08-12 13:20:42 +0200

    fix: ffserver

--- host/ffserver.sh
+++ host/ffserver.sh
@@ -2,8 +2,6 @@
 
 sudo apt install ffmpeg
 
-sudo mkdir -p /etc/ffmpeg/
-
 sudo sh -c 'echo "HTTPPort 8090
 HTTPBindAddress 0.0.0.0
 MaxClients 10
@@ -28,13 +26,13 @@ VideoQMax 10
 <Stream stat.html>
 Format status
 </Stream>
-" > /etc/ffmpeg/ffserver.conf'
+" > /etc/ffserver.conf'
 
 sudo sh -c 'echo "[Unit]
 Wants=network.target
 
 [Service]
-ExecStart=ffserver -f /etc/ffmpeg/ffserver.conf & ffmpeg -v quiet -r 20 -s 1920x1080 -f video4linux2 -i /dev/video0 http://localhost:8090/webcam.ffm
+ExecStart=ffmpeg -v quiet -r 20 -s 1920x1080 -f video4linux2 -i /dev/video0 http://localhost:8090/webcam.ffm
 
 [Install]
 WantedBy=multi-user.target
