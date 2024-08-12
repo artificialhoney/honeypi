4e4a27af1304122cea4d79b5ecc6cf8612292d01
Sebastian Krüger <sk@honeymachine.io>
2024-08-12 07:35:24 +0200

    fix: mediamtx

--- host/mediamtx.sh
+++ host/mediamtx.sh
@@ -4,10 +4,11 @@ sudo apt install libfreetype6 libcamera0
 
 wget -q -O $HOME/mediamtx.tar.gz - https://github.com/bluenviron/mediamtx/releases/download/v1.8.5/mediamtx_v1.8.5_linux_armv6.tar.gz
 tar xzf $HOME/mediamtx.tar.gz
+rm $HOME/mediamtx.tar.gz
 
 sudo cp -r $HOME/mediamtx /opt
 
-ln -s $HONEYPI_MEDIAMTX_CONFIG/mediamtx.yml /opt/mediamtx/mediamtx.yml
+sudo ln -s $HONEYPI_MEDIAMTX_CONFIG/mediamtx.yml /opt/mediamtx/mediamtx.yml
 
 sudo cat << EOT >> /etc/systemd/system/mediamtx.service
 [Unit]
