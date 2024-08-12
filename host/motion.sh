49161ab5c5fb971f39ee26a9453ca38c625db36c
Sebastian Krüger <sk@honeymachine.io>
2024-08-12 11:26:32 +0200

    feat: spotifyd

--- host/motion.sh
+++ host/motion.sh
@@ -3,7 +3,7 @@
 sudo apt-get install libcamera-tools libcamera-v4l2 motion -y
 
 sudo sed -i 's/webcontrol_localhost on/webcontrol_localhost off/g' /etc/motion/motion.conf
-sudo sed -i 's/stream_localhost on/webcontrol_localhost off/g' /etc/motion/motion.conf
+sudo sed -i 's/stream_localhost on/stream_localhost off/g' /etc/motion/motion.conf
 
 sudo sed -i 's/ExecStart=\/usr\/bin\/motion/ExecStart=\/usr\/bin\/libcamerify \/usr\/bin\/motion/g' /lib/systemd/system/motion.service
 
--- host/spotifyd.sh
+++ host/spotifyd.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-wget https://github.com/Spotifyd/spotifyd/releases/download/v0.3.5/spotifyd-linux-armhf-full.tar.gz
+wget https://github.com/Spotifyd/spotifyd/releases/latest/spotifyd-linux-armhf-full.tar.gz
+
+sudo sh -c 'echo "[Unit]
+Description=A spotify playing daemon
+Documentation=https://github.com/Spotifyd/spotifyd
+Wants=sound.target
+After=sound.target
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/usr/bin/spotifyd --no-daemon
+Restart=always
+RestartSec=12
+
+[Install]
+WantedBy=default.target
+" > /etc/systemd/user/spotifyd.service'
+
+# mkdir -p ~/.config/systemd/user/
+# nano ~/.config/systemd/user/spotifyd.service
+# sudo systemctl --user daemon-reload
+
+sudo systemctl --user start spotifyd.service
+
+sudo loginctl enable-linger pi
+sudo systemctl --user enable spotifyd.service
