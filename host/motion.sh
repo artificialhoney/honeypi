ab41296edecd11dc67c30d9002cbbb1d73861d9f
Sebastian Krüger <sk@honeymachine.io>
2024-08-12 10:49:05 +0200

    feat: motion

--- host/motion.sh
+++ host/motion.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# https://lookslikematrix.de/raspberry-pi/2023/10/14/motion-bookworm.html
-sudo apt install libcamera-tools libcamera-v4l2 motion -y
+sudo apt-get install libcamera-tools libcamera-v4l2 motion -y
 sudo libcamerify motion
+
+sudo sed -i 's/webcontrol_localhost on/webcontrol_localhost off/g'
+sudo sed -i 's/stream_localhost on/webcontrol_localhost off/g'
+
+sudo sed -i 's/ExecStart=\/usr\/bin\/motion/ExecStart=\/usr\/bin\/libcamerify \/usr\/bin\/motion/g'
+
+sudo find /var/log/motion -delete
+sudo mkdir /var/log/motion
+sudo chown motion:motion /var/log/motion
+sudo systemctl enable motion.service
+sudo systemctl start motion.service
