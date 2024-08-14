7234fa4b8f9ee0285609a84532ba2755b0405f35
Sebastian Krüger <sk@honeymachine.io>
2024-08-15 01:47:38 +0200

    fix: env vars loading

--- host/home.sh
+++ host/home.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo $HONEYPI_GIT_HOME_URL
+
 cd $HOME
 git init .
-git remote add origin $HONEYPI_HOME_GIT_URL
+git remote add origin "$HONEYPI_GIT_HOME_URL"
 git fetch --all
 git reset --hard origin/main
 cd - 
