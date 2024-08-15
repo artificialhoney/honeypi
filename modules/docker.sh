76209cfe462fd14b0994a7e5b61da8782344a95f
Sebastian Krüger <sk@honeymachine.io>
2024-08-15 19:36:30 +0200

    fix: mediamtx url

--- modules/docker.sh
+++ modules/docker.sh
@@ -5,8 +5,5 @@ curl -fsSL https://get.Docker.com -o get-Docker.sh
 sudo sh get-Docker.sh
 sudo usermod -aG docker $USER
 newgrp docker
-docker run hello-world
-sudo apt-get install docker-compose -y
-
 docker swarm init --advertise-addr eth0
 docker service create --name registry --publish published=5000,target=5000 registry:2
