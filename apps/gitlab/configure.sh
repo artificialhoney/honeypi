31cb107ef5d35137f2bac2c393f34dddd8d14743
Sebastian Krüger <sk@honeymachine.io>
2024-08-15 03:54:35 +0200

    feat: docker swarm (wip)

--- apps/gitlab/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/git
-sudo -u git -H git clone https://gitlab.com/gitlab-org/gitlab-pages.git
-cd gitlab-pages
-sudo -u git -H git checkout v$(</home/git/gitlab/GITLAB_PAGES_VERSION)
-sudo -u git -H make
