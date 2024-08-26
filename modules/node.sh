975bc8affb9307733f04660f6304ed558a35d6e6
Sebastian Krüger <sk@honeymachine.io>
2024-08-26 19:43:58 +0200

    fix: node

--- modules/node.sh
+++ modules/node.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
+export NODE_VER=20.9.0
+
+(cat /proc/cpuinfo | grep -q "Pi Zero") && if [ ! -d node-v${NODE_VER}-linux-armv6l ]; then
+    echo "Installing nodejs ${NODE_VER} for armv6 from unofficial builds..."
+    curl -O https://unofficial-builds.nodejs.org/download/release/v${NODE_VER}/node-v${NODE_VER}-linux-armv6l.tar.xz
+    tar -xf node-v${NODE_VER}-linux-armv6l.tar.xz
+fi
+echo "Adding node to the PATH"
+PATH=$(pwd)/node-v${NODE_VER}-linux-armv6l/bin:${PATH}
