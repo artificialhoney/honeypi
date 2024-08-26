a8ba9092715f4d42ca1bddcd78b888d7876e82ca
Sebastian Krüger <sk@honeymachine.io>
2024-08-26 19:03:50 +0200

    fix: rpiplay

--- modules/node.sh
+++ modules/node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &&\
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
